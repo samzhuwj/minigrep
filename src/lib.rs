d4e3958af19e1c5017c1249b9e14e7e4cfc5c80e
samzhuwj <samzhuwj@gmail.com>
2018-11-06 21:17:05 +0800

    add functionality to see whether the line contains the string in query

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,9 @@ pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     for line in contents.lines() {
-        
+        if line.contains(query) {
+            
+        }
     }
 }
 
