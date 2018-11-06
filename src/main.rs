5d5f1951862dabd2ce80f6b5ed1bb543d59e8b99
samzhuwj <samzhuwj@gmail.com>
2018-11-06 20:37:56 +0800

    exit with an error code if creating a new Config fails

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
