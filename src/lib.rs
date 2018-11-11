c7e9b7ef53b7e0c33b78dac89312442434fce8f9
samzhuwj <samzhuwj@gmail.com>
2018-11-11 21:11:13 +0800

    change the body of Config::new to use iterator methods

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,16 @@ pub struct Config {
 
 impl Config {
     pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config { query, filename, case_sensitive })
     }
